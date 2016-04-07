a71b8dffc3e58236df350b8cdf80f0691d4506aa
basile <basile.duplessis@gmail.com>
2016-04-07 15:01:10 +0200

    Final Akka HTTP template

--- build.sbt
+++ build.sbt
@@ -1,5 +1,16 @@
-name         := "scala-project"
+name         := "akkahttp-project"
 version      := "1.0"
 scalaVersion := "2.11.8"
 
+val akkaVersion  = "2.4.2"
+
 libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.5" % "test"
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http-testkit" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http-experimental" % akkaVersion,
+  "com.typesafe.akka" %% "akka-http-core" % akkaVersion
+  )
+
+
--- src/test/scala/AppSpec.scala
+++ src/test/scala/AppSpec.scala
@@ -1,11 +1,23 @@
-import collection.mutable.Stack
-import org.scalatest._
+import org.scalatest.{Matchers, WordSpec}
+import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.testkit.ScalatestRouteTest
+import akka.http.scaladsl.server._
+import Directives._
+import akkahttp.App.route
+import akka.http.scaladsl.model.StatusCodes._
+import org.scalatest.words.IncludeWord
 
-class AppSpec extends FlatSpec with Matchers {
+class AppSpec extends WordSpec with Matchers with ScalatestRouteTest {
 
-  "Multiplication by 2" should "return same result as addition" in {
-    val number = 2
-    number * 2 should be (number + number)
+  "The service" should {
+
+    "return a greeting for GET requests to the /hello path" in {
+      // tests:
+      Get("/hello") ~> route ~> check {
+        status shouldBe OK
+        responseAs[String] should include("hello")
+      }
+    }
+
   }
-
 }
