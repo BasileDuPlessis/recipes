3661bb98b3c3a5c2278cddd789b402c92bb7c7bb
basile <basile.duplessis@gmail.com>
2016-04-07 17:23:53 +0200

    Add docker support

--- build.sbt
+++ build.sbt
@@ -14,3 +14,6 @@ libraryDependencies ++= Seq(
   )
 
 
+enablePlugins(DockerPlugin)
+
+dockerAutoPackageJavaApplication()
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -24,7 +24,7 @@ object App {
     // needed for the future flatMap/onComplete in the end
     implicit val executionContext = system.dispatcher
 
-    val bindingFuture = Http().bindAndHandle(route, "localhost", 8080)
+    val bindingFuture = Http().bindAndHandle(route, "0.0.0.0", 8080)
 
     println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
     StdIn.readLine() // let it run until user presses return
